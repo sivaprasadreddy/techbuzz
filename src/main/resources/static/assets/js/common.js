1f59e77f9bb07955ced4ebe8840a93c3f43d1f1e
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-02-02 06:49:09 +0530

    Minor UI improvements

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -1,3 +1,4 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
