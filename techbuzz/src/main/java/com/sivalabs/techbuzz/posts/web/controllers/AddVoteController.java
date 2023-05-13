47c6e1fa4502c0c75101c9e48e6f27e7bc64188f
adityachaudhari <chaudhari.aditya24@gmail.com>
2023-05-13 21:27:43 +0530

    formatting changes

--- techbuzz/src/main/java/com/sivalabs/techbuzz/posts/web/controllers/AddVoteController.java
+++ techbuzz/src/main/java/com/sivalabs/techbuzz/posts/web/controllers/AddVoteController.java
@@ -7,27 +7,18 @@ import com.sivalabs.techbuzz.posts.domain.dtos.CreateVoteRequest;
 import com.sivalabs.techbuzz.posts.domain.dtos.PostViewDTO;
 import com.sivalabs.techbuzz.posts.domain.mappers.PostMapper;
 import com.sivalabs.techbuzz.posts.domain.models.Post;
-import com.sivalabs.techbuzz.posts.domain.models.Vote;
 import com.sivalabs.techbuzz.posts.domain.services.PostService;
 import com.sivalabs.techbuzz.users.domain.models.User;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.apache.tomcat.util.http.parser.MediaType;
-import org.codehaus.groovy.util.StringUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @Loggable
@@ -53,8 +44,10 @@ class AddVoteController {
 
     @GetMapping(path = "/api/hx/votes/update/{voteType}/{postId}", produces = "text/html")
     @ResponseBody
-    public String createVoteHTMX(@PathVariable String postId, @PathVariable String voteType, @CurrentUser User loginUser) {
-        CreateVoteRequest createVoteRequest = new CreateVoteRequest(Long.valueOf(postId), loginUser.getId(), Integer.valueOf(voteType));
+    public String createVoteHTMX(
+            @PathVariable String postId, @PathVariable String voteType, @CurrentUser User loginUser) {
+        CreateVoteRequest createVoteRequest =
+                new CreateVoteRequest(Long.valueOf(postId), loginUser.getId(), Integer.valueOf(voteType));
         postService.addVote(createVoteRequest);
         Post updatedPost = postService.getPost(Long.valueOf(postId));
         PostViewDTO postViewDTO = postMapper.toPostViewDTO(loginUser, updatedPost);
@@ -63,7 +56,8 @@ class AddVoteController {
 
         StringBuilder returnHtml = new StringBuilder();
         returnHtml.append("<div id=\"vote-container-" + postId + "\">");
-        returnHtml.append("<button class=\"btn btn-success" + (Integer.valueOf(voteType) == 1 ? " disabled" : "") + "\"");
+        returnHtml.append(
+                "<button class=\"btn btn-success" + (Integer.valueOf(voteType) == 1 ? " disabled" : "") + "\"");
         returnHtml.append(" id=\"upVoteButton\"");
         returnHtml.append(" hx-trigger=\"click\"");
         returnHtml.append(" hx-get=\"/api/hx/votes/update/1/" + postId + "\"");
@@ -73,7 +67,8 @@ class AddVoteController {
         returnHtml.append("<span class=\"p-2 successCount\">" + upVoteCount + "</span>");
         returnHtml.append("<i class=\"fa-solid fa-thumbs-up\"></i>");
         returnHtml.append("</button>");
-        returnHtml.append("<button class=\"btn btn-danger" + (Integer.valueOf(voteType) == -1 ? " disabled" : "") + "\"");
+        returnHtml.append(
+                "<button class=\"btn btn-danger" + (Integer.valueOf(voteType) == -1 ? " disabled" : "") + "\"");
         returnHtml.append(" id=\"downVoteButton\"");
         returnHtml.append(" hx-trigger=\"click\"");
         returnHtml.append(" hx-get=\"/api/hx/votes/update/-1/" + postId + "\"");
@@ -87,10 +82,4 @@ class AddVoteController {
 
         return returnHtml.toString();
     }
-
 }
-
-
-
-
-
