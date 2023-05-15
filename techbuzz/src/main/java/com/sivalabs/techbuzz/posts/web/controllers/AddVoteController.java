75ef81affc020ca0e36de168613c470e90b19baa
adityachaudhari <chaudhari.aditya24@gmail.com>
2023-05-15 20:31:38 +0530

    removed unused imports

--- techbuzz/src/main/java/com/sivalabs/techbuzz/posts/web/controllers/AddVoteController.java
+++ techbuzz/src/main/java/com/sivalabs/techbuzz/posts/web/controllers/AddVoteController.java
@@ -7,12 +7,10 @@ import com.sivalabs.techbuzz.posts.domain.dtos.PostViewDTO;
 import com.sivalabs.techbuzz.posts.domain.services.PostService;
 import com.sivalabs.techbuzz.users.domain.models.User;
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 class AddVoteController {
