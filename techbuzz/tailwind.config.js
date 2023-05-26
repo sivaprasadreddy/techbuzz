5984d8fa4aa0f461756c949ff6a494b4f15d82e4
Pavithra <pavithra@Pavithras-MacBook-Pro.local>
2023-05-26 16:52:37 +0530

    Revert "Tailwind CSS changes"
    This reverts commit 01b9e4c5f3100bf7e3932b9a3145144587f851bc.

--- techbuzz/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/main/resources/templates/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'),],
-}
-
