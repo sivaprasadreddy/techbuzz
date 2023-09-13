8612e1dacfe3f52f24a4bc90424b4a9447fb1a6c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-09-13 17:04:15 +0530

    Adding Taskfile

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@
 
 declare dc_file=deployment/docker-compose/docker-compose.yml
 
-function build_image_jib() {
-    ./mvnw -pl techbuzz clean package -DskipTests jib:dockerBuild -Dimage=sivaprasadreddy/techbuzz
-}
-
 function build_apps_buildpacks() {
     ./mvnw -pl techbuzz clean spring-boot:build-image -Dspring-boot.build-image.imageName=sivaprasadreddy/techbuzz
 }
@@ -13,12 +9,11 @@ function build_apps_buildpacks() {
 function build_apps() {
     ./mvnw -pl techbuzz spotless:apply
     build_apps_buildpacks
-    #build_image_jib
 }
 
 function start_infra() {
     echo "Starting dependent docker containers...."
-    docker-compose -f "${dc_file}" up --build --force-recreate techbuzz-db mailhog -d
+    docker-compose -f "${dc_file}" up --build --force-recreate -d
     docker-compose -f "${dc_file}" logs -f
 }
 
@@ -37,14 +32,14 @@ function restart_infra() {
 function start() {
     echo "Starting app...."
     build_apps
-    docker-compose -f "${dc_file}" up --build --force-recreate -d
-    docker-compose -f "${dc_file}" logs -f
+    docker-compose --profile app -f "${dc_file}" up --build --force-recreate -d
+    docker-compose --profile app -f "${dc_file}" logs -f
 }
 
 function stop() {
     echo 'Stopping app....'
-    docker-compose -f "${dc_file}" stop
-    docker-compose -f "${dc_file}" rm -f
+    docker-compose --profile app -f "${dc_file}" stop
+    docker-compose --profile app -f "${dc_file}" rm -f
 }
 
 function restart() {
