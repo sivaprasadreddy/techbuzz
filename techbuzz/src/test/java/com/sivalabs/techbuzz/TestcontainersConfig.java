1ba2a8f2694bda5a1b241b9cecb97819ce53753a
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-12-10 19:00:33 +0530

    Upgrade versions

--- techbuzz/src/test/java/com/sivalabs/techbuzz/TestcontainersConfig.java
+++ techbuzz/src/test/java/com/sivalabs/techbuzz/TestcontainersConfig.java
@@ -12,7 +12,7 @@ public class TestcontainersConfig {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>("postgres:16.3-alpine");
+        return new PostgreSQLContainer<>("postgres:17-alpine");
     }
 
     /*@Bean
